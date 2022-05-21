0d702817a634b9c74cdbad289f31e321ba964c16
Ar1f007 <muhammadarif9416@yahoo.com>
2022-05-22 05:27:53 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/shared/Navbar';
+
 function App() {
   return (
     <div>
-      <h1 className="text-2xl text-rose-700">Hello World!</h1>
-      <button className="btn btn-primary">Button</button>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/utils/Links.js
@@ -0,0 +1,8 @@
+const links = [
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about' },
+  { name: 'services', path: '/services' },
+  { name: 'Blogs', path: '/blogs' },
+];
+
+export default links;
