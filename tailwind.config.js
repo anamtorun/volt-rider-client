2bd59e0a63c434c80e5b37a56cb56f163a928c48
Ar1f007 <muhammadarif9416@yahoo.com>
2022-05-22 00:40:26 +0600

    integreted tailwind css with daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    // themes: [
+    //   {
+    //     mytheme: {
+    //       primary: '#c64a3f',
+    //       secondary: '#b3ffb2',
+    //       accent: '#edabf4',
+    //       neutral: '#412541',
+    //       'base-100': '#F8F6F8',
+    //       info: '#43A7CB',
+    //       success: '#1BDA8A',
+    //       warning: '#A57403',
+    //       error: '#FB7665',
+    //     },
+    //   },
+    // ],
+  },
+};
