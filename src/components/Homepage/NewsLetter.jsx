a10d0b115d7da10e7b188e22426a2ba5b9fa75b9
Ar1f007 <muhammadarif9416@yahoo.com>
2022-05-26 18:01:38 +0600

    newletter

--- src/components/Homepage/NewsLetter.jsx
+++ src/components/Homepage/NewsLetter.jsx
@@ -1,3 +1,54 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import MySwal from '../../config/sweetAlert';
+
+const schema = yup.object({
+  email: yup
+    .string()
+    .email('Not a valid email')
+    .required('Type your email address')
+    .typeError('Type your email address'),
+});
+
 export const NewsLetter = () => {
-  return <div></div>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = () => {
+    MySwal.fire('', 'Thank you for subscribing to our newsletter', 'success');
+    reset();
+  };
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="container px-4 py-20 lg:py-8 lg:pb-20 mx-auto lg:flex lg:items-center justify-evenly">
+        <h2 className="text-xl font-bold tracking-tight text-gray-600 xl:text-2xl dark:text-white">
+          Join us to get the latest product updates and offers
+        </h2>
+
+        <div className="mt-8 lg:mt-0">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col space-y-3 sm:space-y-0 sm:flex-row sm:-mx-2"
+          >
+            <input
+              {...register('email')}
+              type="text"
+              className="input input-bordered mr-2 w-full min-w-[200px]"
+              placeholder="Email Address"
+            />
+
+            <button className="btn btn-primary">Subscribe</button>
+          </form>
+          {errors?.email && <p className="text-sm text-gray-800 my-2">{errors?.email?.message}</p>}
+        </div>
+      </div>
+    </section>
+  );
 };
